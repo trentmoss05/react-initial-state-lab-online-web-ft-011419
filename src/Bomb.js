4155fbd18339bf42e12d446d73461b143b83361a
Trent Moss <trentmoss05@gmail.com>
2019-05-21 02:26:42 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -11,6 +11,6 @@ class Bomb extends React.Component {
   }
   
   render() {
-    const message = this.state.secondsLeft === 0 ? 'Boom!' : 
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.secondsLeft} seconds before I go boom!`
   }
 }
