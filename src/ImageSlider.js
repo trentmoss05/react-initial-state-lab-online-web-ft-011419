5ebc31013b3676b9615b3d0d2fdea8d5156ffa0c
Trent Moss <trentmoss05@gmail.com>
2019-05-21 02:22:56 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -12,7 +12,7 @@ class ImageSlider extends React.Component {
   
   render() {
     return (
-      <h1>I am on slide {this.state.currentSlideIndex}
-      )
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
   }
 }
